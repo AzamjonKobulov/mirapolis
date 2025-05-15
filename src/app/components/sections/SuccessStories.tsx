15222f075b83f6564bb5e081945eaafbc667da7c
AzamjonKobulov <azamjonkobulov1998@gmail.com>
2025-05-15 18:01:22 +0500

    added/accessible to buttons

--- src/app/components/sections/SuccessStories.tsx
+++ src/app/components/sections/SuccessStories.tsx
@@ -227,7 +227,10 @@ export default function SuccessStories() {
         {/* Navigation Buttons */}
         <div className="flex-center gap-8">
           {/* Next */}
-          <button className="prev size-14 shrink flex-center bg-white rounded-2xl hover:opacity-80 smooth-200 cursor-pointer">
+          <button
+            className="prev size-14 shrink flex-center bg-white rounded-2xl hover:opacity-80 smooth-200 cursor-pointer"
+            aria-label="Previous Slide"
+          >
             <svg
               width="24"
               height="25"
@@ -244,7 +247,10 @@ export default function SuccessStories() {
             </svg>
           </button>
           {/* Prev */}
-          <button className="next size-14 shrink flex-center bg-white rounded-2xl hover:opacity-80 smooth-200 cursor-pointer">
+          <button
+            className="next size-14 shrink flex-center bg-white rounded-2xl hover:opacity-80 smooth-200 cursor-pointer"
+            aria-label="Next Slide"
+          >
             <svg
               width="24"
               height="24"
